0993259a326c52d7b1136d9b0ccf26de3ac88adf
Emilio de Oliveira <emilio.oliveira@safelight.info>
2024-05-04 03:55:08 -0300

    Minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MonitorGuaibaComponent} from './monitor-guaiba/monitor-guaiba.component'
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
